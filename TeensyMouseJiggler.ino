202b553c2bae3fa2307755300213301efd95dc41
Luke Miller <millerlp@gmail.com>
2019-08-05 13:49:46 -0700

    First version
    Jiggles the mouse once per minute for 4 hours, in theory.

---
+++ TeensyMouseJiggler.ino
@@ -0,0 +1,35 @@
+/* TeensyMouseJiggler
+ *  Luke Miller 2019
+ *  
+ *  Code implemented on a Teensy 3.0 to "jiggle" the mouse cursor
+ *  every once in a while (moveInterval) for a set amount of hours (runTimeMillis)
+ *  Once the designated number of hours have elapsed, you will need to manually
+ *  hit the reset button on the Teensy to restart the timer. 
+ *  
+ *  
+ */
+
+#define myLED 13
+
+unsigned long runTimeMillis = 4 * 60 * 60 * 1000; // Number of hours to run this routine (hit reset button on Teensy to restart)
+unsigned int moveInterval = 60 * 1000; // Number of milliseconds between mouse movements
+unsigned int currentMillis; 
+
+void setup() {
+  currentMillis = millis(); // initialize currentMillis
+  pinMode(myLED, OUTPUT);
+  digitalWrite(myLED, LOW);
+  } 
+void loop() {
+  while (millis() < runTimeMillis){
+    if ( (millis() - currentMillis) > moveInterval) {
+      Mouse.move(0,1); // move it
+      delay(10);
+      Mouse.move(0,-1); // move it back    
+      currentMillis = millis(); // update currentMillis
+      digitalWrite(myLED, HIGH);
+      delay(15);
+      digitalWrite(myLED, LOW);
+    }
+  } // end of while loop, reset to restart everything
+}
